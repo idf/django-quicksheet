36a0a9187016245673d98b25e02b36f1053a0197
Daniel D. Zhang <idf@users.noreply.github.com>
2015-08-22 02:07:26 +0800

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -1,9 +1,12 @@
+import re
+from django.http import HttpResponse
+
 def json_only(func):
-  def ret(*args):
+  def ret(*args, **kwargs):
     request = args[0]
     if (request.is_ajax() and
             re.match(r"application/json", request.META["CONTENT_TYPE"])):
-      return func(*args)
+      return func(*args, **kwargs)
     else:
       return HttpResponse("Accepts application/json only")
   return ret
