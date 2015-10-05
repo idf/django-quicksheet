77825dcd6d20344e1e5aa454b4df9ecfcbd1cbeb
Daniel D. Zhang <idf@users.noreply.github.com>
2015-10-04 21:52:19 -0700

    Update django_setup.py

--- django_setup.py
+++ django_setup.py
@@ -1,6 +1,6 @@
 import sys, os, django
 
 home = os.path.expanduser("~")
-sys.path.append(os.path.join(home, 'leetcode/noj'))
+sys.path.append(os.path.join(home, 'path/to/projectroot'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.active")
 django.setup()
